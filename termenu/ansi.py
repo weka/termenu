920a6b36bcd024a5f3987ede8615515a136cf021
Ofer Koren <koreno@gmail.com>
2016-10-27 11:53:21 +0300

    abort write retries after 5 attempts

--- termenu/ansi.py
+++ termenu/ansi.py
@@ -8,12 +8,14 @@ COLORS = dict(black=0, red=1, green=2, yellow=3, blue=4, magenta=5, cyan=6, whit
 
 def write(text):
     def _retry(func, *args):
-        while True:
+        attempts = 5
+        while attempts:
             try:
                 func(*args)
             except IOError as e:
                 if e.errno != errno.EAGAIN:
                     raise
+                attempts -= 1
             else:
                 break
 
